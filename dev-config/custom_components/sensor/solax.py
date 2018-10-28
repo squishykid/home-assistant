9ffbf41b98d7f872bfab68b98ae854e69dddb7f8
Robin Wohlers-Reichel <robin.wohlersreichel@gmail.com>
2018-10-28 23:48:05 +1100

    neat

--- dev-config/custom_components/sensor/solax.py
+++ dev-config/custom_components/sensor/solax.py
@@ -7,29 +7,38 @@ import aiohttp
 import async_timeout
 import voluptuous as vol
 
-from homeassistant.const import TEMP_CELSIUS
+from homeassistant.const import (
+        TEMP_CELSIUS,
+        CONF_TOKEN, CONF_ID
+)
 from homeassistant.helpers.entity import Entity
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 import homeassistant.helpers.config_validation as cv
 from homeassistant.components.sensor import PLATFORM_SCHEMA
+from homeassistant.exceptions import PlatformNotReady
+from homeassistant.helpers.event import async_track_time_interval
 
 _LOGGER = logging.getLogger(__name__)
 
-ATTR_VOLTAGE = 'voltage'
-ATTR_CURRENT = 'current'
-ATTR_POWER = 'power'
-ATTR_TEMPERATURE = 'temperature'
-ATTR_REMAINING_CAPACITY = 'remaining_capacity'
-
-CONF_ID = 'solax_id'
-CONF_TOKEN = 'token'
+ATTR_VOLTAGE = 'Voltage'
+ATTR_CURRENT = 'Current'
+ATTR_POWER = 'Power'
+ATTR_TEMPERATURE = 'Temperature'
+ATTR_REMAINING_CAPACITY = 'Remaining Capacity'
+BATTERY_SENSORS = [
+    ATTR_VOLTAGE,
+    ATTR_CURRENT,
+    ATTR_POWER,
+    ATTR_TEMPERATURE,
+    ATTR_REMAINING_CAPACITY
+]
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_ID): cv.string,
     vol.Required(CONF_TOKEN): cv.string,
 })
 
-SCAN_INTERVAL = timedelta(seconds=30)
+SCAN_INTERVAL = timedelta(minutes=30)
 REQUEST_TIMEOUT = 5
 
 BATTERY_ENDPOINT = 'https://www.solax-portal.com/api/v1/site/BatteryList/{solax_id}?token={token}'
@@ -55,13 +64,20 @@ BATTERY_SCHEMA = vol.Schema({
 
 class SolaxRequestError(Exception):
     """Error to indicate a Solax API request has failed."""
-
     pass
 
 
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
     """Setup the sensor platform."""
-    async_add_entities([Battery(hass, config.get(CONF_ID), config.get(CONF_TOKEN))])
+    update_interval = timedelta(seconds=30)
+    endpoint = BatteryEndpoint(hass, config.get(CONF_ID), config.get(CONF_TOKEN))
+    hass.async_add_job(endpoint.async_refresh)
+    async_track_time_interval(hass, endpoint.async_refresh, update_interval)
+    devices = []
+    for x in BATTERY_SENSORS:
+        devices.append(Battery(endpoint, x))
+    endpoint.sensors = devices
+    async_add_entities(devices)
 
 
 async def async_solax_dashboard_request(hass, schema, solax_id, token, retry, wait_time=0):
@@ -112,7 +128,7 @@ def parse_solax_battery_response(json):
     }
 
 
-class Battery(Entity):
+class BatteryEndpoint:
     """Representation of a Sensor."""
 
     def __init__(self, hass, solax_id, token):
@@ -120,38 +136,58 @@ class Battery(Entity):
         self.hass = hass
         self.solax_id = solax_id
         self.token = token
-        self._state = {}
+        self.data = {}
+        self.ready = asyncio.Event()
+        self.sensors = []
 
-
-    @property
-    def name(self):
-        """Return the name of the sensor."""
-        return 'Example Temperature'
-
-    @property
-    def state(self):
-        """Return the state of the sensor."""
-        return self._state.get(ATTR_REMAINING_CAPACITY, None)
-    
-    @property
-    def device_state_attributes(self):
-        return {
-            ATTR_VOLTAGE: self._state.get(ATTR_VOLTAGE, None),
-            ATTR_CURRENT: self._state.get(ATTR_CURRENT, None),
-            ATTR_POWER: self._state.get(ATTR_POWER, None),
-            ATTR_TEMPERATURE: self._state.get(ATTR_TEMPERATURE, None),
-        }
-
-    @property
-    def unit_of_measurement(self):
-        """Return the unit of measurement."""
-        return '%'
-
-    async def async_update(self):
+    async def async_refresh(self, now=None):
         """Fetch new state data for the sensor.
 
         This is the only method that should fetch new data for Home Assistant.
         """
-        json = await async_solax_dashboard_request(self.hass, BATTERY_SCHEMA, self.solax_id, self.token, 3)
-        _LOGGER.info("called async_update %s", json)
-        self._state = parse_solax_battery_response(json)
+        try:
+            json = await async_solax_dashboard_request(self.hass, BATTERY_SCHEMA, self.solax_id, self.token, 3)
+            _LOGGER.info("called async_update %s", json)
+            self.data = parse_solax_battery_response(json)
+            self.ready.set()
+        except SolaxRequestError:
+            if now is not None:
+                self.ready.clear()
+            else:
+                raise PlatformNotReady
+        for s in self.sensors:
+            s.async_schedule_update_ha_state(force_refresh=True)
+
+
+class Battery(Entity):
+    def __init__(self, endpoint, key):
+        self._endpoint = endpoint
+        self._key = key
+        self._value = None
+    
+    @property
+    def state(self):
+        _LOGGER.warn('state')
+        return self._value
+
+    @property
+    def name(self):
+        return self._key
+
+    @property
+    def unit_of_measurement(self):
+        """Return the unit of measurement."""
+        return {
+            ATTR_VOLTAGE: 'V',
+            ATTR_CURRENT: 'A',
+            ATTR_POWER: 'W',
+            ATTR_TEMPERATURE: TEMP_CELSIUS,
+            ATTR_REMAINING_CAPACITY: '%'
+        }[self._key]
+    
+    async def async_update(self):
+        """Update station state."""
+        _LOGGER.warn('async_update')
+        if self._endpoint.ready.is_set():
+            if self._key in self._endpoint.data:
+                self._value = self._endpoint.data[self._key]
